786328f4582d595a28652113c585866856642430
William Jackson <william@subtlecoolness.com>
2021-01-26 15:12:05 -0600

    Add configuration for web server threads

--- birthdayfeed/config.py
+++ birthdayfeed/config.py
@@ -10,3 +10,4 @@ class Config:
         self.log_format = os.getenv('LOG_FORMAT', '%(levelname)s [%(name)s] %(message)s')
         self.log_level = os.getenv('LOG_LEVEL', 'INFO')
         self.version = os.getenv('APP_VERSION', 'unknown')
+        self.web_server_threads = int(os.getenv('WEB_SERVER_THREADS', '8'))
